e90ed88658acbd07c675d9fb39ee137822759f28
Erik Aasen <erikaase@stud.ntnu.no>
2026-02-13 11:22:18 +0100

    oprettet share klassen

--- src/main/java/org/edu/ntnu/idatt2003/group49/millions/Share.java
+++ src/main/java/org/edu/ntnu/idatt2003/group49/millions/Share.java
@@ -1,7 +1,28 @@
 package org.edu.ntnu.idatt2003.group49.millions;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Share {
-  private  ;
-  private
-public Share()
+  private Stock stock;
+  private BigDecimal quantity;
+  private BigDecimal purchasePrice;
+
+  public Share(Stock stock, BigDecimal quantity, BigDecimal purchasePrice) {
+    this.stock = Objects.requireNonNull(stock, "stock cannot be null");
+    this.quantity = Objects.requireNonNull(quantity, "quantity cannot be null");
+    this.purchasePrice = Objects.requireNonNull(purchasePrice, "quantity cannot be null");
+  }
+
+  public Stock getStock() {
+    return stock;
+  }
+
+  public BigDecimal getQuantity() {
+    return quantity;
+  }
+
+  public BigDecimal getPurchasePrice() {
+    return purchasePrice;
+  }
 }
