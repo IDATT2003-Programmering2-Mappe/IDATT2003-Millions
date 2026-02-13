8a543afb1dcaa78cc0373b934a7d30a741238e99
Emil-F <e.fagerjord@gmail.com>
2026-02-13 10:56:59 +0100

    init: Opprettet Calculator klassene

---
+++ src/main/java/org/edu/ntnu/idatt2003/group49/millions/PurchaseCalculator.java
@@ -0,0 +1,33 @@
+package org.edu.ntnu.idatt2003.group49.millions;
+
+import java.math.BigDecimal;
+
+public class PurchaseCalculator implements TransactionCalculator {
+  private BigDecimal purchasePrice;
+  private BigDecimal quantity;
+
+  public PurchaseCalculator(Share share) {
+    this.purchasePrice = share.getPurchasePrice();
+    this.quantity       = share.getQuantity();
+  }
+
+  @Override
+  public BigDecimal calculateGross() {
+    return null;
+  }
+
+  @Override
+  public BigDecimal calculateCommission() {
+    return null;
+  }
+
+  @Override
+  public BigDecimal calculateTax() {
+    return null;
+  }
+
+  @Override
+  public BigDecimal calculateTotal() {
+    return null;
+  }
+}
