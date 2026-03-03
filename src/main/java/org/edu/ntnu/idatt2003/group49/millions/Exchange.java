d870b3cea686c844318bca055959f544e08e46ba
Emil-F <e.fagerjord@gmail.com>
2026-03-03 16:30:24 +0100

    feat:  begynnelse på buy og sell metodene i Exchange

--- src/main/java/org/edu/ntnu/idatt2003/group49/millions/Exchange.java
+++ src/main/java/org/edu/ntnu/idatt2003/group49/millions/Exchange.java
@@ -1,8 +1,10 @@
 package org.edu.ntnu.idatt2003.group49.millions;
 
+import org.edu.ntnu.idatt2003.group49.millions.calculator.PurchaseCalculator;
 import org.edu.ntnu.idatt2003.group49.millions.model.Player;
 import org.edu.ntnu.idatt2003.group49.millions.model.Share;
 import org.edu.ntnu.idatt2003.group49.millions.model.Stock;
+import org.edu.ntnu.idatt2003.group49.millions.transaction.Purchase;
 import org.edu.ntnu.idatt2003.group49.millions.transaction.Transaction;
 
 import java.math.BigDecimal;
@@ -30,30 +32,49 @@ public class Exchange {
   }
 
   public boolean hasStock(String symbol) {
+    if (symbol == null) {
+      throw new IllegalArgumentException("symbol cannot be null");
+    }
     return stockMap.containsKey(symbol);
   }
 
   public Stock getStock(String symbol) {
+    if (symbol == null) {
+      throw new NullPointerException("symbol cannot be null");
+    }
     return stockMap.get(symbol);
   }
 
   public List<Stock> findStocks(String searchTerm) {
+    if (searchTerm == null) {
+      throw new NullPointerException("searchTerm cannot be null");
+    }
     String lowerCaseSearch = searchTerm.toLowerCase();
 
     return stockMap.values().stream().filter(stock ->
-            stock.getSymbol().contains(lowerCaseSearch)
-                    || stock.getCompany().contains(lowerCaseSearch)).toList();
+        stock.getSymbol().toLowerCase().contains(lowerCaseSearch) ||
+        stock.getCompany().toLowerCase().contains(lowerCaseSearch)).toList();
   }
 
   public Transaction buy(String symbol, BigDecimal quantity, Player player) {
-    return null;
+    if (!hasStock(symbol)) {
+      throw new IllegalArgumentException("Cannot purchase a share of a stock that does not exist within the Exchange");
+    }
+    Stock stock = getStock(symbol);
+    Share share = new Share(stock, quantity, stock.getSalesPrice());
+    Purchase purchase = new Purchase(share, getWeek(), new PurchaseCalculator(share));
+    purchase.commit(player);
+    return purchase;
   }
 
   public Transaction sell(Share share, Player player) {
-      if (!player.getPortfolio().contains(share)) {
-        throw new IllegalArgumentException("player cannot sell a share they do not own.");
-      }
-      return null;
+    if (share == null ||  player == null) {
+      throw new NullPointerException("share or player cannot be null");
+    }
+    if (!player.getPortfolio().contains(share)) {
+      throw new IllegalArgumentException("player cannot sell a share they do not own.");
+    }
+    return null;
   }
 
   public void advance() {
