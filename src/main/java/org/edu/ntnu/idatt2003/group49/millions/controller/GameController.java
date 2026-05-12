5fcfa450fdc014625925c6c288b7fa5e046939df
Erik Aasen <erikaase@stud.ntnu.no>
2026-05-12 14:09:09 +0200

    start new game method

--- src/main/java/org/edu/ntnu/idatt2003/group49/millions/controller/GameController.java
+++ src/main/java/org/edu/ntnu/idatt2003/group49/millions/controller/GameController.java
@@ -1,24 +1,49 @@
 package org.edu.ntnu.idatt2003.group49.millions.controller;
 
 import org.edu.ntnu.idatt2003.group49.millions.model.GameSession;
+import org.edu.ntnu.idatt2003.group49.millions.model.exchange.Exchange;
+import org.edu.ntnu.idatt2003.group49.millions.model.exchange.Stock;
+import org.edu.ntnu.idatt2003.group49.millions.model.player.Player;
+import org.edu.ntnu.idatt2003.group49.millions.utils.io.MillionsFileReader;
 
-import java.io.FileReader;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
 public class GameController {
   private GameSession activeSession;
-  private final FileReader fileReader;
+  private final MillionsFileReader fileReader;
 
-  public GameController(FileReader fileReader) {
+  public GameController(MillionsFileReader fileReader) {
     this.fileReader = Objects.requireNonNull(fileReader, "fileReader requires non null");
   }
+
   public Optional<GameSession> getActiveSession() {
     return Optional.ofNullable(activeSession);
+  }
+
+  public Optional<String> startNewGame(String name, BigDecimal startingMoney, Path path) {
+    try{
+      Player player = new Player(name, startingMoney);
+      List<Stock> stocks = fileReader.readStocks(path);
+      Exchange exchange = new Exchange("S&P 500", stocks);
+
+      activeSession = new GameSession(player, exchange);
+
+      return Optional.empty();
+
+    } catch (Exception e) {
+      return Optional.of("Could not start game");
+    }
+
   }
 
   public void clearSession() {
     activeSession = null;
   }
+
+
 
 }
