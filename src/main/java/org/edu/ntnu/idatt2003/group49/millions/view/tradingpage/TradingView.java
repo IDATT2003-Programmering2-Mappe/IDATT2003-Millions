3741e9efa7e9063a9c5cfce10b77b9b5e7dcbd6e
Emil-F <e.fagerjord@gmail.com>
2026-05-13 10:30:05 +0200

    feat: created trading view

---
+++ src/main/java/org/edu/ntnu/idatt2003/group49/millions/view/tradingpage/TradingView.java
@@ -0,0 +1,15 @@
+package org.edu.ntnu.idatt2003.group49.millions.view.tradingpage;
+
+import javafx.scene.layout.Pane;
+import org.edu.ntnu.idatt2003.group49.millions.view.MillionsView;
+
+public class TradingView extends MillionsView {
+  public TradingView() {
+    getChildren().add(build());
+  }
+
+  @Override
+  protected Pane build() {
+    return null;
+  }
+}
